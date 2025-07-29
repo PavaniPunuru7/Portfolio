94c222d24a452b8fb84961ec840b4667699850fb
PavaniPunuru7 <punurupavani7@gmail.com>
2025-07-29 11:44:06 +0530

    updated profile picture

--- src/components/HeroSection/HeroStyle.js
+++ src/components/HeroSection/HeroStyle.js
@@ -94,7 +94,7 @@ export const HeroRightContainer = styled.div`
 export const Img = styled.img`
   position: relative;
   width: 100%;
-  height: 100%;
+  height: 60%;
   max-width: 400px;
   max-height: 400px;
   border-radius: 50%;
